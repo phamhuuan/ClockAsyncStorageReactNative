427314a9d8b62f1f2b70441e815156eca1389b5b
phamhuuan <phaman3899@gmail.com>
2020-02-22 02:00:48 +0700

    handle result in ShowTime component

--- src/components/StopWatch/ShowTime.js
+++ src/components/StopWatch/ShowTime.js
@@ -1,13 +1,26 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
+import {useDispatch, useSelector} from 'react-redux';
+import {handleTime} from './handle';
 
-export default function ShowTime(props) {
-	const {stopWatch} = props;
+export default function ShowTime() {
+	const time = useSelector(state => state.stopWatchReducer.time);
+	const isStart = useSelector(state => state.stopWatchReducer.isStart);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		let interval;
+		if (isStart) {
+			interval = setInterval(() => {
+				dispatch({type: 'TIMING'});
+			}, 50);
+		}
+		return () => clearInterval(interval);
+	}, [dispatch, isStart]);
 	return (
 		<View style={styles.body}>
-			<Text style={{fontSize: 50}}>{stopWatch}</Text>
+			<Text style={{fontSize: 50}}>{handleTime(time / 100)}</Text>
 		</View>
 	);
 }
